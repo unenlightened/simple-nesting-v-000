7f2089b6633904fbd9b88a4b05d960cbc6d556c9
Chely Ho <adept_inept@yahoo.com>
2018-02-08 19:42:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages][0]
+		 programmer_hash[:dennis_ritchie][:languages].join(", ")
 end
 
 def adding_matz
